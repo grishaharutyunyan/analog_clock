005fc2c5f9fdfae04b94571b5ecff1614f84b97b
grishaharutyunyan <grishaharutyunian@gmail.com>
2024-01-25 23:53:25 +0400

    js task

---
+++ script.js
@@ -0,0 +1,19 @@
+setInterval(() => {
+    const time = document.querySelector(".display #time");
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+
+    time.textContent = hours + ":" + minutes + ":" + seconds;
+}, 100);
